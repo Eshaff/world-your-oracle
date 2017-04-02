13bad2d87f2c1d1e12235fc1ccd35078f324e0bb
Dominic Hartjes <dhartjes@gmail.com>
2017-04-02 14:48:03 -0500

    Prior to switch to webpack

---
+++ app/js/events.js
@@ -0,0 +1,15 @@
+require(["jquery", "kendo.window"], function ($, kendoWindow) {
+	//console.log(kendo);
+
+//	$(".myKendoWindow").kendoWindow({
+	//	width: "600px",
+		//title: "About Alvar Aalto",
+		//visible: false,
+		//actions: [
+			//"Pin",
+			//"Minimize",
+			//"Maximize",
+			//"Close"
+		//]
+	//}).data("kendoWindow").center().open();
+});
--- app/js/main.js
+++ app/js/main.js
@@ -1,53 +1,8 @@
-// Quote rotator jquery library
+define(['jquery'], function ($) {
+    $(document).ready(function () {
 
-//Quotes rotator
-var divs = $('.cbp-qtcontent');
-
-var dots = $('.dots');
-
-function advance(){
-    var current = $('.current');
-    var currentIndex = divs.index(current),
-        nextIndex = currentIndex + 1;
-
-    if (nextIndex >= divs.length) {
-        nextIndex = 0;
-    }
-
-    var next = divs.eq(nextIndex);
-
-    fade(current, next);
-}
-
-function retreat(){
-    var current = $('.current');
-    var currentIndex = divs.index(current),
-        nextIndex = currentIndex - 1;
-
-    if (nextIndex < 0) {
-        nextIndex = divs.length -1;
-    }
-
-    var next = divs.eq(nextIndex);
-
-    fade(current, next)
-}
-
-function fade(current, next) {
-    next.stop().fadeIn(1500, function () {
-        $(this).addClass('current');
-    });
-
-    current.stop().fadeOut(1500, function () {
-        $(this).removeClass('current');
-    });
-}
-
-// Events
-function nextPane() {
-    advance();
-}
-
-function previousPane() {
-    retreat();
-}
+        require(['js/layout', 'js/rotator'], function (layout, rotator, events) {
+            layout();
+        });
+    })
+});
