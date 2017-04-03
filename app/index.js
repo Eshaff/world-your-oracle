1413b08bd1d7223d2e3630c2bac2292ed55781a1
Dominic Hartjes <dhartjes@gmail.com>
2017-04-02 21:48:39 -0500

    eventCalendar as amd module - working with 1 error

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 var $ = require('jquery')
+var jQuery = require('jquery')
 
+var eventCalendar = require('jquery.eventCalendar')
 require('kendo.dropdownlist')
 
 $("#ddl").kendoDropDownList({
@@ -13,7 +15,24 @@ $("#ddl").kendoDropDownList({
 
 $(document).ready(function () {
 	var layout = require('js/layout');
+	// Rotator for the Oracle.htm page
 	require('js/rotator');
 
+	// Header and footer layout
 	layout();
+
+	eventCalendar(jQuery);
+
+	// EventCalendar for the Calendar page
+	$("#eventCalendarDefault").eventCalendar({
+		eventsjson: 'app/js/events.json', // link to events json
+		eventsLimit: 10,
+		startWeekOnMonday: false,
+		showDescription: true,
+		openLinkInNewWindow: false,
+		dateFormat: 'MMM D, YYYY',
+		jsonDateFormat: 'human'
+	});
 });
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,32 @@
-var path = require('path');
+var path = require("path");
+var webpack = require("webpack");
 
 module.exports = {
 	resolve: {
-		extensions: ['.js', 'min.js' ],
+		extensions: [".js", "min.js"],
 		modules: [
 			//"node_modules",
 			path.resolve(__dirname, "app"),
 			path.resolve(__dirname, "app/lib"),
-			path.resolve(__dirname, "app/lib/kendo-ui-core/js")
+			path.resolve(__dirname, "app/lib/kendo-ui-core/js"),
+			path.resolve(__dirname, "app/lib/jQueryEventCalendar/js")
+			//<script src="app/lib/jQueryEventCalendar/js/moment.js" type="text/javascript"></script>
+			// <script src="app/lib/jQueryEventCalendar/js/jquery.eventCalendar.js" type="text/javascript"></script>
+
+
 
 
 		]
 	},
-	entry: './app/index.js',
+	entry: "./app/index.js",
 	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
-	}
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist")
+	},
+	plugins: [
+		new webpack.ProvidePlugin({
+			$: "jquery",
+			jQuery: "jquery"
+		})
+	]
 };
